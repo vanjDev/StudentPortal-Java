9d3fe8dc6561087f0803143967475c04946da398
Aero <143190477+vanjDev@users.noreply.github.com>
2024-11-16 08:06:47 +0800

    Update Course.java

--- Course.java
+++ Course.java
@@ -9,10 +9,24 @@ public class Course {
         this.units = units;
     }
 
+    void show(){
+        System.out.println("This is a course!");
+    }
+
     public String getCourseCode() {
         return courseCode;
     }
     public int getUnits() {
         return units;
+    }
+}
+
+class GED extends Course {
+    public GED(String courseCode, int units) {
+        super(courseCode, units);
+    }
+        @Override
+    void show(){
+        System.out.println("This is GED course, therefore it's accessible to other programs.");
     }
 }
